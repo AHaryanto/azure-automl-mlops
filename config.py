99d1ea1c638b239fbf303d6d534d27363dade565
Alvin Haryanto <haryanto.alvin@gmail.com>
2022-07-27 14:26:24 -0700

    Provision a Compute object

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import sys
 from azureml.core import VERSION, Workspace
 from azureml.core.authentication import (InteractiveLoginAuthentication,
                                          ServicePrincipalAuthentication)
-from azureml.core.compute import ComputeTarget
+from azureml.core.compute import AmlCompute, ComputeTarget
+from azureml.core.compute_target import ComputeTargetException
 from azureml.core.conda_dependencies import CondaDependencies
 from azureml.core.runconfig import CondaDependencies, RunConfiguration
 from loguru import logger
@@ -40,9 +41,30 @@ else:
 ws = Workspace.from_config(auth=auth)
 logger.debug(f"Found workspace {ws.name} at location {ws.location}")
 
-compute_target = ComputeTarget(
-    workspace=ws,
-    name=params["compute_name"])
+try:
+    # Check for existing compute target
+    compute_target = ComputeTarget(
+        workspace=ws,
+        name=params["compute_name"])
+    logger.debug(f'Found existing "{params["compute_name"]}" cluster.')
+except ComputeTargetException:
+    # If it doesn't already exist, create it
+    try:
+        logger.debug('Creating a new compute cluster...')
+        compute_config = AmlCompute.provisioning_configuration(
+            vm_size=params['vm_size'],
+            min_nodes=0,
+            max_nodes=1,
+            idle_seconds_before_scaledown=120,
+            location=ws.location)
+        pipeline_cluster = ComputeTarget.create(
+            ws,
+            name=params["compute_name"],
+            provisioning_configuration=compute_config)
+        pipeline_cluster.wait_for_completion(show_output=True)
+        logger.debug('Compute cluster created successfully.')
+    except Exception as ex:
+        logger.debug(ex)
 
 # Printing file systems
 logger.debug("These are the connected file systems available now:")
